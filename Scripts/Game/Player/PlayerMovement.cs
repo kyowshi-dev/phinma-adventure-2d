1c8ca90faf700bbc24506da0ad1ded41b02cbcf6
Charlz <chsa.chavaria.coc@phinmaed.com>
2026-02-08 19:36:04 +0800

    Add new scene and update existing assets
    - Created a new scene file "Scene 1" with associated metadata.
    - Modified "Test.unity" to include additional MeshRenderer and MeshFilter components for improved rendering.
    - Enhanced "FieldOfView.cs" to dynamically generate a mesh based on field of view parameters.
    - Updated "PlayerMovement.cs" to include animation handling for player movement.
    - Adjusted texture import settings for multiple metal and stain textures to disable wrapping and change sprite mesh type.

--- Scripts/Game/Player/PlayerMovement.cs
+++ Scripts/Game/Player/PlayerMovement.cs
@@ -19,17 +19,27 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 _smoothedMovementInput;
     private Vector2 _movementInputSmoothVelocity;
     private Camera _camera;
+    private Animator _animator;
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
         _camera = Camera.main;
+        _animator = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
     {
         SetPlayerVelocity();
         RotateInDirectionOfInput();
+        SetAnimation();
+    }
+
+    private void SetAnimation()
+    {
+        bool isMoving = _movementInput != Vector2.zero;
+
+        _animator.SetBool("IsMoving", isMoving);
     }
 
     private void SetPlayerVelocity()
